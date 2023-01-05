f82899b8e14dd7f05befd2a380053b52437c79af
KyleKotowick <kotowick@invictonlabs.com>
2023-01-05 13:23:27 -0500

    Formatting; use of local vars

--- variables.tf
+++ variables.tf
@@ -3,11 +3,17 @@ variable "working_dir" {
   type        = string
   default     = null
 }
+locals {
+  var_working_dir = var.working_dir
+}
 
 variable "fetch" {
   description = "Whether to run `git fetch` prior to reading the Git metadata."
   type        = bool
   default     = false
+}
+locals {
+  var_fetch = var.fetch != null ? var.fetch : false
 }
 
 variable "pull" {
@@ -15,11 +21,17 @@ variable "pull" {
   type        = bool
   default     = false
 }
+locals {
+  var_pull = var.pull != null ? var.pull : false
+}
 
 variable "get_current_branch" {
   description = "Whether to get the currently checked out branch."
   type        = bool
   default     = true
+}
+locals {
+  var_get_current_branch = var.get_current_branch != null ? var.get_current_branch : true
 }
 
 variable "get_local_branches" {
@@ -27,11 +39,17 @@ variable "get_local_branches" {
   type        = bool
   default     = true
 }
+locals {
+  var_get_local_branches = var.get_local_branches != null ? var.get_local_branches : true
+}
 
 variable "get_remote_branches" {
   description = "Whether to get a list of all remote branches."
   type        = bool
   default     = true
+}
+locals {
+  var_get_remote_branches = var.get_remote_branches != null ? var.get_remote_branches : true
 }
 
 variable "get_commit_hash" {
@@ -39,11 +57,17 @@ variable "get_commit_hash" {
   type        = bool
   default     = true
 }
+locals {
+  var_get_commit_hash = var.get_commit_hash != null ? var.get_commit_hash : true
+}
 
 variable "get_current_tags" {
   description = "Whether to get the list of tags associated with the currently checked out commit."
   type        = bool
   default     = true
+}
+locals {
+  var_get_current_tags = var.get_current_tags != null ? var.get_current_tags : true
 }
 
 variable "get_tags" {
@@ -51,9 +75,15 @@ variable "get_tags" {
   type        = bool
   default     = true
 }
+locals {
+  var_get_tags = var.get_tags != null ? var.get_tags : true
+}
 
 variable "get_remotes" {
   description = "Whether to get a list of all available remotes and their fetch/push URLs."
   type        = bool
   default     = true
+}
+locals {
+  var_get_remotes = var.get_remotes != null ? var.get_remotes : true
 }
